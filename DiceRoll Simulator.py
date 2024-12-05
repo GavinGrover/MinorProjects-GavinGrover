8e42788f75b222adafb67473f8076f9a7059a893
Gavin Grover <groverrgavin@gmail.com>
2024-12-05 20:01:37 +0530

    Dice Roll Simulator
    This is created with the help of random module in python

---
+++ DiceRoll Simulator.py
@@ -0,0 +1,12 @@
+#Dice Simulator
+import random
+
+while True:
+    toRoll = str(input("Enter (y/n) to roll a dice = "))
+    if toRoll in ["y", "Y"]:
+        print("You rolled", random.randint(1, 6))
+    elif toRoll in ["n", "N"]:
+        print("You are being Exited")
+        exit()
+    else:
+        print("Invalid input, please enter 'y' or 'n'.")
